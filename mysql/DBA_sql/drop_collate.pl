2b5e8f302f770576ecb1b04a94ab6187ec1ccd04
huangchunhui <Alex@alex-mac.local>
2015-12-20 13:29:12 +0800

    modify mysql config template

--- mysql/DBA_sql/drop_collate.pl
+++ mysql/DBA_sql/drop_collate.pl
@@ -1,8 +1,10 @@
 #!/usr/bin/perl -w
 
+# step 0 : alter table collate to 
+# select concat('ALTER TABLE ',TABLE_SCHEMA,'.',TABLE_NAME,' COLLATE=utf8_general_ci;')  from tables where TABLE_COLLATION='utf8_bin' and TABLE_SCHEMA not in('mysql','information_schema','performance_schema');
 
 # step 1 : use sql get all column name which is not default collate
-# select TABLE_SCHEMA,TABLE_NAME,COLUMN_NAME,CHARACTER_SET_NAME,COLLATION_NAME,COLUMN_TYPE from columns where CHARACTER_SET_NAME='utf8' and  column_type like '%char%' and table_schema not in ('information_schema','mysql','performance_schema') and COLLATION_NAME <>'utf8_general_ci'
+# select TABLE_SCHEMA,TABLE_NAME,COLUMN_NAME,CHARACTER_SET_NAME,COLLATION_NAME,COLUMN_TYPE from columns where CHARACTER_SET_NAME='utf8' and (column_type like '%char%' or column_type like '%text%') and table_schema not in ('information_schema','mysql','performance_schema') and COLLATION_NAME <>'utf8_general_ci'
 
 # step 2 : use mysqldump get DDL from mysql
 # mysqldump -uroot -ppass -S/data/mysql/mysql.sock --skip-triggers --no-data --no-create-db --skip-opt --all-databases > stable.sql
